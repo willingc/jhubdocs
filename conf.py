d228e368981f9b8e2a093cfb4f19b2adf615e169
Carol Willing <carolcode@willingconsulting.com>
2016-09-09 18:39:46 -0700

    Edit conf.py

--- conf.py
+++ conf.py
@@ -359,11 +359,8 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-#    import sphinx_rtd_theme
-#    html_theme = 'classic'
-#     html_theme = 'scrolls'
+    import cloud_sptheme as csp
     html_theme = 'cloud'
     html_theme_path = [csp.get_theme_dir()]
     html_theme_options = { "roottarget": "index" }
-#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
